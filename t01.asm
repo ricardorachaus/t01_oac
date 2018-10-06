0cb9919d756da8dba469fa87f618d3e61b62e1b2
Ricardo Rachaus <ricardorachaus@gmail.com>
2018-10-06 14:09:33 -0300

    Changing read file

--- t01.asm
+++ t01.asm
@@ -5,6 +5,10 @@ invalid_option: .asciiz "\nOption selected is invalid! Try another.\n"
 
 img_name: .asciiz "img.bmp"
 header: .space 54
+size: .space 5
+width: .space 4
+height: .space 4
+trash: .space 30
 
 open_error_msg: .asciiz "Error opening the file.\n"
 read_error_msg: .asciiz "Error reading the file.\n"
@@ -55,20 +59,63 @@ read_img:
 	syscall
 	blt $v0, $zero, read_file_error		# Check if there is a error reading.
 	
-	# Store the width
-	lw 		$s7, header + 18
-	mul		$s7, $s7, 3
+	# Get image size
+	li $v0, 14
+	la $a1, size
+	li $a2, 4
+	syscall
 	
-	# Store the height
-	lw		$s4, header + 22
-	# Store the size of the data
-	lw		$s1, header + 34
+	# Trash to be ignored from header
+	li $v0, 14
+	la $a1, trash
+	li $a2, 12
+	syscall
 	
-	# Allocate memory in heap
-	#li $v0, 9
-	#move $a0, $s1
-	#syscall
+	# Get image width
+	li $v0, 14
+	la $a1, width
+	li $a2, 4
+	syscall
 	
+	# Get image height
+	li $v0, 14
+	la $a1, height
+	li $a2, 4
+	syscall
+	
+	# Trash to be ignored
+	li $v0, 14
+	la $a1, trash
+	li $a2, 28
+	syscall
+	
+	# Calculate image size by width x height
+	# lw $s3, width
+	# lw $s4, height
+	# mul $t1, $s3, $s4
+	# mul $t1, $t1, 4
+	
+	# # Allocate image size in the heap
+	# li $v0, 9
+	# move $a0, $t1
+	# syscall
+	
+	# # Stores the heap address
+	# move $s1, $v0
+	
+	# # Calculate heap size of information
+	# ld $t1, size
+	# addi $t1, $t1, -54 # Image size without header
+	# sd $t1, size
+	
+	# # Allocate size of the information
+	# li $v0, 9
+	# move $a0, $t1
+	# syscall
+	
+	# # Stores the information addres
+	# move $s2, $v0
+
 	j main
 
 save_img:
