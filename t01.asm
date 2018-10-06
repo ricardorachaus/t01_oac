ff531c16d57d0902a13b64864226843e38f4cd0b
Ricardo Rachaus <ricardorachaus@gmail.com>
2018-10-06 15:09:26 -0300

    Read the image and display

--- t01.asm
+++ t01.asm
@@ -4,14 +4,18 @@ menu_text: .asciiz "Choose a option:\n1 - Read image\n2 - Save image\n3 - Blur e
 invalid_option: .asciiz "\nOption selected is invalid! Try another.\n"
 
 img_name: .asciiz "img.bmp"
-header: .space 54
-size: .space 5
+type: .space 2
+	.align 2
+size: .space 6
+	.align 2
 width: .space 4
 height: .space 4
 trash: .space 30
 
 open_error_msg: .asciiz "Error opening the file.\n"
 read_error_msg: .asciiz "Error reading the file.\n"
+
+debug_msg: .asciiz "Error!\n"
 	
 .text
 
@@ -51,11 +55,12 @@ read_img:
 	syscall
 	blt $v0, $zero, open_file_error		# Check if there is a error opening.
 	
-	# Read the image
+	# Read the image for type
 	move $a0, $v0
+	move $s0, $v0
 	li $v0, 14
-	la $a1, header
-	la $a2, 54
+	la $a1, type
+	la $a2, 2
 	syscall
 	blt $v0, $zero, read_file_error		# Check if there is a error reading.
 	
@@ -90,33 +95,95 @@ read_img:
 	syscall
 	
 	# Calculate image size by width x height
-	# lw $s3, width
-	# lw $s4, height
-	# mul $t1, $s3, $s4
-	# mul $t1, $t1, 4
+	lw $s3, width
+	lw $s4, height
+	mul $t1, $s3, $s4
+	mul $t1, $t1, 4
 	
-	# # Allocate image size in the heap
-	# li $v0, 9
-	# move $a0, $t1
-	# syscall
+	# Allocate image size in the heap
+	li $v0, 9
+	move $a0, $t1
+	syscall
 	
-	# # Stores the heap address
-	# move $s1, $v0
+	# Stores the heap address
+	move $s1, $v0
 	
-	# # Calculate heap size of information
-	# ld $t1, size
-	# addi $t1, $t1, -54 # Image size without header
-	# sd $t1, size
+	# Calculate heap size of information
+	ld $t1, size
+	addi $t1, $t1, -54 # Image size without header
+	sd $t1, size
 	
-	# # Allocate size of the information
-	# li $v0, 9
-	# move $a0, $t1
-	# syscall
+	# Allocate size of the information
+	li $v0, 9
+	move $a0, $t1
+	syscall
 	
-	# # Stores the information addres
-	# move $s2, $v0
+	# Stores the information address
+	move $s2, $v0
+
+	j load_image_to_be_display
 
 	j main
+
+# Load the image by each pixel to be displayed.
+load_image_to_be_display:
+	# Handle the width data of the image
+	move $t5, $s3
+	mulu $t0, $s3, 3
+	add $t1, $t0, $t0
+
+	# Calculate the pixels in image
+	mulu $t2, $s3, $s4
+	mulu $t3, $t2, 4
+	move $s5, $t3
+
+	# Handle the height data of the image
+	mulu $t2, $s3, $s4
+	mulu $t3, $t2, 3
+	move $t2, $s2
+	addu $t2, $t3, $t2
+
+	# Read what remains of the image
+	li $v0, 14
+	move $a0, $s0
+	move $a2, $t3
+	move $a1, $s2
+	syscall
+
+	move $t3, $s4 # Columns quantity
+	move $t4, $s1
+	subu $t2, $t2, $t0
+
+	j read_column
+
+# Change to 4 bytes and store image
+read_column: 	beqz $t5, read_row
+	jal read_pixel
+	sw $t6, ($t4)
+	addi $t4, $t4, 4
+	addi $t2, $t2, 3
+	addi $t5, $t5, -1
+	j read_column
+		
+# Change to next row
+read_row:
+	beqz $t3, main
+	sub $t2, $t2, $t1
+	move $t5, $s3
+	addi $t3, $t3, -1
+	j read_column
+
+# Read each pixel
+read_pixel:
+	move $t6, $zero
+	lbu $t6, ($t2)
+	lbu $t8, 1($t2)
+	sll $t8, $t8, 8
+	or $t6, $t6, $t8
+	lbu $t8, 2($t2)
+	sll $t8, $t8, 16
+	or $t6, $t6, $t8
+	jr $ra
 
 save_img:
 	j main
@@ -146,6 +213,18 @@ read_file_error:
 # Print any string stored in $a0
 print_str:
 	li $v0, 4
+	syscall
+	jr $ra
+	
+# Print any integer stored in $a0
+print_int:
+	li $v0, 1
+	syscall
+	jr $ra
+
+debug:
+	li $v0, 4
+	la $a0, debug_msg
 	syscall
 	jr $ra
 
